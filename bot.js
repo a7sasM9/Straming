993c1549bce96c9e650e4254f91eaccf0afdcac7
a7sasM9 <50416386+a7sasM9@users.noreply.github.com>
2019-05-10 00:29:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
 }
 
 if (message.content.startsWith(prefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/overwatchleague");
+  client.user.setGame(argresult, "https://www.twitch.tv/abu_droush");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
