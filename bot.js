a117314962513cd165e161f88188b91441793491
a7sasM9 <50416386+a7sasM9@users.noreply.github.com>
2019-05-10 21:58:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', message => {
       message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+    client.user.setGame(argresult, "https://www.twitch.tv/abu_droush");
       message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
